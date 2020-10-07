9f8237927af91a0e5fa4ce8f1a427eb99b60fb74
root <prabhatkmr5789@gmail.com>
2020-10-07 23:39:49 +0530

    align activity card to left side

--- code/SubsetSum.java
+++ code/SubsetSum.java
@@ -22,26 +22,38 @@ public class SubsetSum {
 
         Scanner input = new Scanner(System.in);
         System.out.println("enter size of array");
-        int n = input.nextInt()
-        int arr[] = new int [n];
+        int n = 6;
+        int arr[] = {3, 34, 4, 12, 5, 2 };
         System.out.println("enter sum");
-        int sum = input.nextInt();
-        System.out.println("enter element in array to find out the sum of sub array");
-        for(int i=0;i<n;i++){
-            arr[i] = input.nextInt();
+        int sum = 9;
+       
+        boolean [][] dp = new boolean [n+1][sum+1];
+        for(int i=0;i<n+1;i++){
+            for(int j=0;j<sum+1;j++){
+                if(i==0){
+                    dp[i][j] = false;
+                }
+                if(j==0){
+                    dp[i][j] = true;
+                }
+            }
         }
-        SubsetSum sb = new SubsetSum(n, sum);
-        
         for(int i=1;i<n+1;i++){
             for(int j=1;j<sum+1;j++){
-                if(arr[i]<=sum){
+                
+                if(arr[i-1]<=j){
                     dp[i][j] = dp[i-1][j-arr[i-1]] || dp[i-1][j];
+                }else{
+                    dp[i][j] = dp[i-1][j];
                 }
-                dp[i][j] = dp[i-1][j];
+                
             }
         }
 
-        System.out.println(dp[n][sum]);
+        
+            System.out.println(dp[n][sum]);
+        
+        input.close();
 
     }
     
